cb0a154c9120b69b133e2a5d4cdb4beb1020ae54
Aryaman Sharda <asharda@ucsd.edu>
2015-02-11 10:49:31 -0800

    Changes.

--- public/js/index.js
+++ public/js/index.js
@@ -14,17 +14,35 @@ function initializePage() {
 }
 
 function register(e) {
-	//window.alert(document.getElementById("username").value);
-	//window.alert(document.getElementById("pwd").value);
+	window.alert(document.getElementById("username").value);
+	window.alert(document.getElementById("pwd").value);
 
-	window.alert($document.getElementById("username").value);
-	window.alert($document.getElementById("pwd").value);
+	//window.alert($document.getElementById("username").value);
+	//window.alert($document.getElementById("pwd").value);
 
-	window.alert("1" + document.getElementById("#username").value);
-	window.alert("2" + document.getElementById("#pwd").value);
+	//window.alert("1" + document.getElementById("#username").value);
+	//window.alert("2" + document.getElementById("#pwd").value);
 
 	var dec = document.getElementById("textBox1").value;
-	if (strValue) {
+
+	/*if (validateEmail()) {
     	window.location.href = "register";
-	}   
+	}   */
 }
+
+//Regex validation
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
+
+function validateUsername(username) { 
+    var re = /^[a-z0-9_-]{3,16}$/;
+    return re.test(username);
+} 
+
+function validatePassword(password) { 
+    var re = /^[a-z0-9_-]{6,18}$/;
+    return re.test(password);
+} 
+
