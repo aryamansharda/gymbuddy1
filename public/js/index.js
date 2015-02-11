eba14ca0f53620216365d3ae255c040b1a8df083
Aryaman Sharda <asharda@ucsd.edu>
2015-02-11 10:24:13 -0800

    Testing functionality

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,7 @@ function initializePage() {
 }
 
 function register(e) {
-    //window.location.href = "register";
-    alert("Text: " + $("#username").text());
-    alert("Text: " + $("#pwd").text());
+	var word = document.getElementById("username").value;
+	alert(escape(word));
+    window.location.href = "register";
 }
