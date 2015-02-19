c0a61f3923acb19722413dfb6340e9b9b99883d6
vagrant <vagrant@precise32.(none)>
2015-02-19 18:26:34 +0000

    updated styles on pages

---
+++ public/js/myprofiles.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+                  initializePage();
+                  })
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+    $("#backButton").click(projectClick);
+}
+
+function projectClick(e) {
+    // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+    window.location.href = "/dashboard";
+}
