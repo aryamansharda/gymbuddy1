136b2411d924da8b6bc8f6314b49d1761bbcdcc9
Aryaman Sharda <asharda@ucsd.edu>
2015-02-26 17:18:40 -0800

    Dashboard v2

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         var currentAttrValue = jQuery(this).attr('href');
  
         // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+        jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
  
         // Change/remove current tab to active
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
